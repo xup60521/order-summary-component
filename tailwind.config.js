7e0f1d9cef564f198fea435aa8b23458001fa904
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-19 13:10:51 +0800

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Pale_blue: "hsl(225, 100%, 94%)",
+                c_Bright_blue: "hsl(245, 75%, 52%)",
+                c_Very_pale_blue: "hsl(225, 100%, 98%)",
+                c_Desaturated_blue: "hsl(224, 23%, 55%)",
+                c_Dark_blue: "hsl(223, 47%, 23%)",
             },
             fontFamily: {
-
+                red_hat_display: ["Red Hat Display", "sans-serif"]
             }
         },
     },
